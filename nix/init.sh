489d7a1c28a8d93255c926d4aebfb65e824a4103
Kamil Alekberov <kamil.alekberov@avisbudget.com>
2025-12-06 22:07:49 +0500

    feat: allow boot partition

--- nix/init.sh
+++ nix/init.sh
@@ -7,12 +7,16 @@ DISK=/dev/sda
 # Create a new GPT partition table
 parted $DISK -- mklabel gpt
 
+# Create a BIOS Boot Partition (1MiB) - type EF02 (required for BIOS mode with GPT)
+parted $DISK -- mkpart primary 1MiB 2MiB
+parted $DISK -- set 1 bios_grub on
+
 # Create an EFI System Partition (ESP) - 512MiB, type EF00 (vfat)
-parted $DISK -- mkpart ESP fat32 1MiB 512MiB
-parted $DISK -- set 1 boot on
+parted $DISK -- mkpart ESP fat32 2MiB 514MiB
+parted $DISK -- set 2 boot on
 
 # Create a Linux swap partition (optional but recommended) - e.g., 8GiB
-parted $DISK -- mkpart primary linux-swap 512MiB -8GiB
+parted $DISK -- mkpart primary linux-swap 514MiB -8GiB
 
 # Create the main Linux filesystem (root) partition, using the rest of the space
 parted $DISK -- mkpart primary ext4 -8GiB 100%
@@ -22,14 +26,14 @@ lsblk $DISK
 
 
 # Format the EFI partition as FAT32
-mkfs.fat -F 32 -n EFI_BOOT ${DISK}1
+mkfs.fat -F 32 -n EFI_BOOT ${DISK}2
 
 # Format the swap partition and enable it
-mkswap -L SWAP ${DISK}2
-swapon ${DISK}2
+mkswap -L SWAP ${DISK}3
+swapon ${DISK}3
 
 # Format the root partition as ext4 (or btrfs/zfs if your config specifies it)
-mkfs.ext4 -L NIXOS_ROOT ${DISK}3
+mkfs.ext4 -L NIXOS_ROOT ${DISK}4
 
 sleep 2
 
